9777aa5596c7de2126a97c07eccea69f40fe8d22
Teszáry Péter <peterteszary@gmail.com>
2023-06-28 21:53:38 +0200

    First Java Class of my Private Summer Camp

---
+++ Java Programming Masterclass/HelloWorld/src/IfThenStatement.java
@@ -0,0 +1,22 @@
+public class IfThenStatement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Peter");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("It is not an alien!");
+            System.out.println("It is not an alien!");
+        }
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+    }
+}
