7fe2098aeb08402308c596c3f1c5b3ef37d0fbc9
Teszáry Péter <peterteszary@gmail.com>
2023-06-29 21:55:32 +0200

    1 step

--- Java Programming Masterclass/HelloWorld/src/IfThenStatement.java
+++ Java Programming Masterclass/HelloWorld/src/IfThenStatement.java
@@ -21,5 +21,34 @@ public class IfThenStatement {
         if ((topScore > 90) || (secondTopScore <= 90)) {
             System.out.println("Either or both of the conditions are true");
         }
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is True!");
+        }
+        boolean isCar = false;
+        if (isCar == true) {
+            System.out.println("This is not supposed to happen");
+
+        }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = myFirstValue + mySecondValue * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("TheRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+
     }
 }
