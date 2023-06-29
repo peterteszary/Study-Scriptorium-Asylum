4376819d3a86cb9ed74589c4410b74ff4556d0a7
Teszáry Péter <peterteszary@gmail.com>
2023-06-29 05:26:06 +0200

    1 step

--- Java Programming Masterclass/HelloWorld/src/IfThenStatement.java
+++ Java Programming Masterclass/HelloWorld/src/IfThenStatement.java
@@ -18,5 +18,8 @@ public class IfThenStatement {
         if (topScore > secondTopScore && topScore < 100) {
             System.out.println("Greater than second top score and less than 100");
         }
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
     }
 }
