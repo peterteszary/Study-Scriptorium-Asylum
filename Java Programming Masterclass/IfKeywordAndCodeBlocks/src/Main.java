60b6a438e8288ed514c484cf469e951add6a9ffb
Teszáry Péter <peterteszary@gmail.com>
2023-08-15 14:23:49 +0200

    If else

---
+++ Java Programming Masterclass/IfKeywordAndCodeBlocks/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score <= 5000) {
+            System.out.println("Your score was less than or equal to 5000");
+        } else {
+            System.out.println("Got here");
+        }
+
+    }
+}
