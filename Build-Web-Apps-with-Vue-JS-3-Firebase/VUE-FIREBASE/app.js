a7091be5489e7e4d90913f13f6ae9acbc793f025
Teszáry Péter <peterteszary@gmail.com>
2023-11-01 18:06:19 +0100

    Commit

---
+++ Build-Web-Apps-with-Vue-JS-3-Firebase/VUE-FIREBASE/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'The lost empire',
+            author: 'Teszáry Péter',
+            age: 5
+        }
+    }
+})
+
+app.mount('#app')
