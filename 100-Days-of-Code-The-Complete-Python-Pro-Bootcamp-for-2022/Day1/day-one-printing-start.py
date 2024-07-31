2f686b7551168e3513c00527f7122beb94a48015
Teszáry Péter <peterteszary@gmail.com>
2024-07-31 06:23:01 +0200

    day2 commits

---
+++ 100-Days-of-Code-The-Complete-Python-Pro-Bootcamp-for-2022/Day1/day-one-printing-start.py
@@ -0,0 +1,9 @@
+#Print modifiers
+
+print("Hello World!")
+print("Azt mondom 'Hello', mert ki kell ezt próbálni.")
+print('Azt mondom "Hello", mert ki kell ezt próbálni.')
+print('Azt mondom \"Hello\", mert ki kell ezt próbálni.')
+
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " Peter!")
